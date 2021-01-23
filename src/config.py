96ae74f343844c15bb25901466b68656f6207108
AlphaBeta <farizi1906@gmail.com>
2021-01-23 12:28:25 +0700

    Delete config.py

--- src/config.py
+++
@@ -1,3 +0,0 @@
-weather_key =  "http://api.openweathermap.org/data/2.5/weather?appid=22d01d1910218ea2d5703df319c156b3"
-covid_key = "0743a20e0bmshbb24509ac54e2b9p1a1a88jsn53bf7fcc82d0"
-jokes_key = "0743a20e0bmshbb24509ac54e2b9p1a1a88jsn53bf7fcc82d0"
