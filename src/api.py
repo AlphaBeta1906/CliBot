b824af226f9404822a065d90271aa895eb8b20bb
Alpha <farizi1906@gmail.com>
2021-01-26 17:41:25 +0700

    add new command in WebScrapping.py,exception handling in api.py and add colorama in main.py

--- src/api.py
+++ src/api.py
@@ -12,20 +12,27 @@ def Handle(url):
 			return "not found"
 		else:
 			return NotFound
-	except Exc:
+	except:
 		return NotFound
 
 def UselessFact():
-	url = "https://uselessfacts.jsph.pl/random.json?language=en"
-	myDict = Handle(url)
-	print("here a random useless fact : " + myDict['text'] + "\n")
+	try:
+		url = "https://uselessfacts.jsph.pl/random.json?language=en"
+		myDict = Handle(url)
+		print("here a random useless fact : " + myDict['text'] + "\n")
+	except TypeError:
+		print("error")
+
 
 def bored():
-	url = "https://www.boredapi.com/api/activity"
-	myDict = Handle(url)
-	print("are you bored? try this activity : \n" + "activity name : "+ myDict['activity'])
-	print("type : " + myDict['type'])
-	print("number of partcipant : " + str(myDict['participants']))
-	print("price : " + str(myDict['price']))
-	link = myDict['link'] if myDict['link'] != "" else "none"
-	print("link : " + link + "\n")
+	try:
+		url = "https://www.boredapi.com/api/activity"
+		myDict = Handle(url)
+		print("are you bored? try this activity : \n" + "activity name : "+ myDict['activity'])
+		print("type : " + myDict['type'])
+		print("number of partcipant : " + str(myDict['participants']))
+		print("price : " + str(myDict['price']))
+		link = myDict['link'] if myDict['link'] != "" else "none"
+		print("link : " + link + "\n")
+	except TypeError:
+		print("error")
